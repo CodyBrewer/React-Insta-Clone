9d2850f0cf23eaf35c04634c0e3688c6663e2fc1
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-16 12:20:41 -0600

    removed scsss created search form component

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import './SearchBar.css';
 import { Instagram } from 'react-feather';
+
 
 const SearchBar = props => {
     return(
-        <Instagram />
+        <>
+            <Instagram />
+            <span>|</span>
+            <h1>Instagram</h1>
+
+        </>
     )
 }
 
---
+++ instagram/src/components/SearchBar/SearchForm.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {  Search } from 'react-feather';
+
+const SearchForm = props => {
+    return(
+        <form action="">
+            <input type="search" name="" id=""/>
+        </form>
+    )
+}
