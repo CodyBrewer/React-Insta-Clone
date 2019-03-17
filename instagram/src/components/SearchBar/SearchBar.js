417cd9a2ee4bb756f618ccddd199648ad57ee725
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-17 17:21:18 -0600

    added scss + some styling on search bar/form

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,17 +1,24 @@
 import React from 'react';
-import './SearchBar.css';
-import { Instagram } from 'react-feather';
+import './SearchBar.scss';
+import { Instagram, Compass, Heart, User} from 'react-feather';
 import SearchForm from './SearchForm';
 
 
 const SearchBar = props => {
     return(
-        <>
-            <Instagram />
-            <span>|</span>
-            <h1>Instagram</h1>
+        <div className="searchbar-container">
+            <div className="searchbar-logo">
+                <Instagram />
+                <span>|</span>
+                <h1>Instagram</h1>
+            </div>
             <SearchForm />
-        </>
+            <div className="searchbar-user-icons">
+                <Compass />
+                <Heart />
+                <User />
+            </div>
+        </div>
     )
 }
 
--- instagram/src/components/SearchBar/SearchForm.js
+++ instagram/src/components/SearchBar/SearchForm.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import {InputGroup, InputGroupAddon,Input } from 'reactstrap';
 import {  Search } from 'react-feather';
 
 const SearchForm = props => {
     return(
-        <form action="">
-
-    <input type="search" name="" id="" placeholder={<Text>&#128197</Text>} search/>
-        </form>
+        <div>
+            <InputGroup>
+            <InputGroupAddon addonType="append">
+                    <Search className="icon-search"/>
+                <Input placeholder="Search" id="search-input"/>
+                </InputGroupAddon>
+            </InputGroup>
+        </div>
     )
 }
 export default SearchForm;
