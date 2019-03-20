a2e3f5f5fc300e378574f8da9b502242831362c1
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-19 19:55:16 -0600

    created and implemented withAuthenticate HOC

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -1,14 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
-
-
 import PostsPage from './components/PostContainer/PostsPage';
+import withAuthenticate from './components/authentication/withAuthenticate';
+
+const ComponentFromWithAuthenticate = withAuthenticate(PostsPage);
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-          <PostsPage />
+          <ComponentFromWithAuthenticate />
       </div>
     );
   }
