95188c4f415e4e0ce164fc1b65ce69c48db11d32
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-22 21:13:38 -0600

    add styled-componenets/styled-icons refactor search bar logo

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -1,9 +1,38 @@
 import React, {Component} from "react";
 import "./SearchBar.scss";
-import { Instagram } from "react-feather";
 import SearchForm from "./SearchForm";
 import SearchBarIcons from "./SearchBarIcons";
+import styled from "styled-components";
+import { Instagram } from "styled-icons/icomoon/Instagram";
 
+const Header = styled.header`
+width: 100%;
+display: flex;
+justify-content: space-around;
+border: 1px solid #d3d3d3;
+margin: 15px 0;
+box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
+`
+const LogoHeader = styled.div`
+  width: 33%;
+  display: flex;
+  line-height: 75px;
+  align-items: center;
+`;
+
+const Divider = styled.span`
+  font-size: 40px;
+  margin-bottom: 8px;
+`
+const LogoInstagram = styled(Instagram)`
+  height: 50%;
+`
+
+const BrandName = styled.h1`
+  font-size: 40px;
+  font-family: "Satisfy", cursive;
+  margin-top: 15px;
+`
 class SearchBar extends Component{
   constructor(props){
     super(props);
@@ -14,17 +43,23 @@ class SearchBar extends Component{
     window.location.reload();
   }
 
+
   render(){
     return (
-      <div className="searchbar-container">
-        <div className="searchbar-logo">
-          <Instagram />
-          <span>|</span>
-          <h1>Instagram</h1>
-        </div>
-        <SearchForm searchPosts={this.props.searchPosts} />
-        <SearchBarIcons logOut={this.logOut} />
-      </div>
+      <Header>
+        <LogoHeader>
+          <LogoInstagram />
+          <Divider>|</Divider>
+          <BrandName>Instagram</BrandName>
+
+        </LogoHeader>
+        <SearchForm 
+          searchPosts={this.props.searchPosts}
+          />
+        <SearchBarIcons 
+          logOut={this.logOut} 
+          />
+      </Header>
     );
   }
 };
