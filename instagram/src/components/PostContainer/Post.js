6a8757d019c082ccfa5c5c3cfad055e029610171
CodyBrewer <brewer.cody.m@gmail.com>
2019-03-22 22:53:11 -0600

    fixed margin between posts

--- instagram/src/components/PostContainer/Post.js
+++ instagram/src/components/PostContainer/Post.js
@@ -7,7 +7,7 @@ import styled from 'styled-components';
 
 const PostBorder = styled.div`
   border: 1px solid #d3d3d3;
-  margin: 1px 0;
+  margin: 40px 0;
   box-shadow: 0px 3px 15px rgba(0, 0, 0, 0.2);
 `
 const PostImage = styled.img`
